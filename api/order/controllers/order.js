9f3761792ee1f297966cef5f386e09b6c79dccc2
entryone <a.i.kulinich@gmail.com>
2021-01-28 23:36:44 +0200

    test

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -5,4 +5,52 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async addFromCart(ctx) {
+    console.log('data', ctx.request.body)
+    const prods = {}
+    const products = ctx.request.body.items.map(item => item.product)
+    const pr = await strapi.query('product')
+      .find({id_in: products})
+    pr.forEach(prod => {
+      prods[prod.id] = prod
+    })
+    let fullSum = 0
+    let fullTitle = []
+    let slug = ''
+    let totalSumText = ''
+    ctx.request.body.items.forEach(function (item) {
+      const product = prods[item.product]
+      item.Price = product.Price
+      const itemSum = product.Price * item.Count
+      item.sum = `${product.Name}: ${product.Price} x ${item.Count} = ${itemSum} руб`
+      fullSum += itemSum
+      fullTitle.push(`${product.Name} x ${item.Count}`)
+      slug = `${fullTitle.join(', ')}`
+      totalSumText = `${fullSum} руб`
+    })
+    const order = await strapi.query('order').create({
+      slug,
+      totalSumText,
+      itemmm: ctx.request.body.items,
+    });
+    await senEmail()
+    return {
+      id: order.id
+    };
+  },
+};
+
+
+async function senEmail () {
+  await strapi.plugins['email'].services.email.send({
+    to: 'a.i.kulinich@gmail.com',
+    from: 'admin@strapi.io',
+    subject: 'Comment posted that contains a bad words',
+    text: `
+          The comment #555 contain a bad words.
+
+          Comment:
+        `,
+  });
+}
--- api/order/models/order.js
+++ api/order/models/order.js
@@ -6,43 +6,5 @@
  */
 
 module.exports = {
-  lifecycles: {
-    beforeUpdate: (params, data) => handleOrder(data),
-    //beforeCreate: data => handleOrder(data),
-    afterFindOne: viewOrder,
-    afterFind: viewOrder,
-    afterSearch: viewOrder,
-    afterCountSearch: viewOrder,
-  },
+  lifecycles: {},
 };
-
-
-async function handleOrder (data) {
-  let fullSum = 0
-  let fullTitle = []
-  data.itemmm.forEach(async item => {
-    const product = await strapi.query('product').findOne({ id: item.product })
-    const itemSum = item.Price * item.Count
-    item.sum = `${product.Name}: ${item.Price} x ${item.Count} = ${itemSum} руб`
-    fullSum += itemSum
-    fullTitle.push(`${product.Name} x ${item.Count}`)
-    data.slug = `${fullTitle.join(', ')}`
-    data.totalSumText = `${fullSum} руб`
-  })
-}
-
-
-async function viewOrder (data) {
-  console.log('data', data)
-  let fullSum = 0
-  let fullTitle = []
-  data.itemmm.forEach(item => {
-    const product = item.product
-    const itemSum = item.Price * item.Count
-    item.sum = `${product.Name}: ${item.Price} x ${item.Count} = ${itemSum} руб`
-    fullSum += itemSum
-    fullTitle.push(`${product.Name} x ${item.Count}`)
-    data.slug = `${fullTitle.join(', ')}`
-    data.totalSumText = `${fullSum} руб`
-  })
-}
