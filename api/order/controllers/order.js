8d92ce3f375cb2754520a01a02156a2779881f47
entryone <a.i.kulinich@gmail.com>
2021-01-31 00:43:05 +0200

    test

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,4 +1,14 @@
 'use strict';
+const TelegramBot = require('node-telegram-bot-api');
+const token = '756258457:AAEsOHkTF61dZ_-_tCsMkmaCMuLykp06pGQ';
+const bot = new TelegramBot(token, {polling: true});
+const kulinich_chatId = '429692685'
+bot.on('message', (msg) => {
+  console.log(msg);
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  bot.sendMessage(chatId, 'Received your message: '+text, {parse_mode: 'html'});
+});
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
@@ -27,7 +37,7 @@ module.exports = {
       item.sum = `${product.Name}: ${product.Price} x ${item.Count} = ${itemSum} руб`
       fullSum += itemSum
       fullTitle.push(`${product.Name} x ${item.Count}`)
-      description = `${fullTitle.join(', ')}`
+      description = `${fullTitle.join(', \n')}`
       totalSumText = `${fullSum} руб`
     })
 
@@ -48,14 +58,20 @@ module.exports = {
 
 
 async function senEmail (order) {
+  const text = `===========
+№ <b>${order.id}</b>
+${order.description}
+Сумма: ${order.totalSumText}
+Имя: ${order.customerName}
+Телефон: ${order.phone}
+Адрес: ${order.address}
+===========`
+  bot.sendMessage(kulinich_chatId, text, {parse_mode: 'html'});
   return await strapi.plugins['email'].services.email.send({
     to: 'a.i.kulinich@gmail.com',
     from: 'a.i.kulinich@gmail.com',
     subject: 'Новый заказа №' + order.id,
-    text: `${order.description}
-Сумма: ${order.totalSumText}
-Имя: ${order.customerName}
-Телефон: ${order.phone}
-Адрес: ${order.address}`,
+    text,
   });
+
 }
