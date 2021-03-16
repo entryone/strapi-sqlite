82d0a3737c6a2f0faafb81f5ee05b5a2bfc7d8dd
entryone <a.i.kulinich@gmail.com>
2021-03-16 09:09:27 +0200

    moved to GC

--- config/database.js
+++ config/database.js
@@ -4,6 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
+        client: 'postgres',
+        host: `${env('INSTANCE_CONNECTION_NAME')}`,
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+      },
+      options: {},
+    },
+    sqlite: {
+      connector: 'bookshelf',
+      settings: {
         client: 'sqlite',
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
